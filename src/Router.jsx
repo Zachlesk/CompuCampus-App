83eba539f280bebd66219b7c732378b50a64a479
Shadaz <126103144+NicolasMachadoX@users.noreply.github.com>
2023-10-18 17:35:35 -0500

    feat(Add Vacant Component): :sparkles:

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,14 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HomeView } from "./views/Home";
 import { Agregar } from "./components/Agregar/Agregar";
 import { OfertaAplicada } from "./components/Cards/OfertaAplicada/OfertaAplicada";
+import { Vacante } from "./components/Vacante/Vacante";
 
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomeView />} />
-        <Route path="/post" element={<Agregar></Agregar>}/>
+      <Route path="/" element={<HomeView/>}/>
+        <Route path="/vacante" element={<Vacante/>} />
+        <Route path="/post" element={<Agregar/>}/>
         <Route path="/oferta" element={<OfertaAplicada/>}/>
       </Routes>
     </BrowserRouter>
