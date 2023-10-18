f9176ca863c056fbbd48cfb54ad7758678d7ecce
Nicolas Ramirez <nicolas.casanova20@gmail.com>
2023-10-18 13:41:49 -0500

    feat: :rocket: estructura conexion y carga tablas

---
+++ controller/camper.js
@@ -0,0 +1,17 @@
+
+
+export async function postCamper(){
+const {id_camper,nombre_camper,cv, id_ruta, id_grupo}=req.body
+const datos={id_camper,nombre_camper,cv, id_ruta, id_grupo};
+    con.query(/*sql */ `INSERT INTO tipo_documento SET ?`,[datos], (err,data,fil)=>{
+        if (err) {
+            console.error("Error al ejecutar la consulta de inserción: ", err);
+            res.status(500).send("Error al ejecutar la consulta de inserción");
+            return;
+        }
+
+    console.log("post camper");
+    res.send(JSON.stringify(data));
+    console.log(data);
+    })
+}
