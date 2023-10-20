6346a5c0251519fd7e2ff9a42cf25bf75c92bf77
Shadaz <126103144+NicolasMachadoX@users.noreply.github.com>
2023-10-19 19:15:01 -0500

    feat: :twisted_rightwards_arrows: I adapted the components
    adapted vacante, ofertaAplicada y agregar components

--- src/components/Agregar/Agregar.jsx
+++ src/components/Agregar/Agregar.jsx
@@ -1,38 +1,41 @@
 import React, { useEffect, useState } from 'react';
 import './Agregar.css';
+import axios from 'axios';
 
 export const Agregar = () => {
-  const [data, setData] = useState([]);
   const [title, setTitle] = useState('');
   const [sueldo, setSueldo] = useState('');
   const [jornada, setJornada] = useState('');
   const [descripcion, setDescripcion] = useState('');
-  const [activation, setActivation] = useState(false);
 
-  useEffect(() => {
-    if (activation) {
-    
-      const dataHandler = () => {
-        const postData = {
-          title,
-          sueldo,
-          jornada,
-          descripcion
-        };
-        console.log(postData);
-      };
-
-      dataHandler();
-    }
-  }, [activation, title, sueldo, jornada]);
-
-  const handlePublicarClick = (e) => {
+  const handlePublicarClick = async (e) => {
     e.preventDefault();
-    setActivation(true); // Toggle activation to true
+
+    const postData = {
+      title,
+      sueldo,
+      jornada,
+      descripcion
+    };
+
+    try {
+      const response = await axios.post("http://127.0.0.1:0000/api/ofertas/add", postData);
+
+   
+      console.log("Solicitud POST exitosa:", response.data);
+
+     
+      setTitle('');
+      setSueldo('');
+      setJornada('');
+      setDescripcion('');
+    } catch (error) {
+      console.error("Error al realizar la solicitud POST:", error);
+    }
   };
 
   return (
-    <form className=''>
+    <form>
       <div className='buttonDiv'>
         <button onClick={handlePublicarClick} className="buttonForm">
           <p>Publicar</p>
@@ -43,23 +46,23 @@ export const Agregar = () => {
         <div className='formContent'>
           <div className='up'>
             <label className='labelForm'>Titulo</label>
-            <input className='inputTitle' name='titulo' onChange={(e) => setTitle(e.target.value)} />
+            <input className='inputTitle' name='titulo' value={title} onChange={(e) => setTitle(e.target.value)} />
 
             <div className='upDivision'>
               <div className='marginForm'>
                 <label className='labelForm'>Sueldo</label>
-                <input className='inputForm' name="sueldo" onChange={(e) => setSueldo(e.target.value)} />
+                <input className='inputForm' name="sueldo" value={sueldo} onChange={(e) => setSueldo(e.target.value)} />
               </div>
 
               <div>
                 <label className='labelForm'>Jornada</label>
-                <input className='inputForm' name="jornada" onChange={(e) => setJornada(e.target.value)} />
+                <input className='inputForm' name="jornada" value={jornada} onChange={(e) => setJornada(e.target.value)} />
               </div>
             </div>
           </div>
           <div className='down'>
             <label className='labelForm'>Descripcion</label>
-            <textarea name='descripcion' className='inputFormDescripcion' onChange={(e) => setDescripcion(e.target.value)} placeholder=' # Título  ## Subtítulo '></textarea>
+            <textarea name='descripcion' className='inputFormDescripcion' value={descripcion} onChange={(e) => setDescripcion(e.target.value)} placeholder=' # Título  ## Subtítulo '></textarea>
           </div>
         </div>
       </div>
